645f4e3bcea891ee2510d7ea3efefb10b8f4caae
anshumanr <rawat.anshuman@gmail.com>
2022-06-10 12:54:29 -0400

    removed route 53 calls

--- main.go
+++ main.go
@@ -4,10 +4,8 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/route53"
 	"github.com/aws/aws-sdk-go/service/sts"
 )
 
@@ -34,7 +32,6 @@ func getIamIdentity(session *session.Session) {
 		}
 	}
 
-	fmt.Println("Caller identity: (go string) ", result.GoString())
 	fmt.Println("Caller identity: (string) ", result.String())
 
 	data, err := os.ReadFile("/var/run/secrets/eks.amazonaws.com/serviceaccount/token")
@@ -42,54 +39,4 @@ func getIamIdentity(session *session.Session) {
 		fmt.Println("Error reading file: ", err.Error())
 	}
 	fmt.Println("File content: ", string(data))
-}
-
-func addPod(session *session.Session) {
-	svc := route53.New(session)
-	input := &route53.ChangeResourceRecordSetsInput{
-		ChangeBatch: &route53.ChangeBatch{
-			Changes: []*route53.Change{
-				{
-					Action: aws.String("CREATE"),
-					ResourceRecordSet: &route53.ResourceRecordSet{
-						Name: aws.String("_sip._udp.example.com"),
-						ResourceRecords: []*route53.ResourceRecord{
-							{
-								Value: aws.String("1 10 5080 192.168.1.100"),
-							},
-						},
-						TTL:  aws.Int64(60),
-						Type: aws.String("SRV"),
-					},
-				},
-			},
-			Comment: aws.String("FS instance"),
-		},
-		HostedZoneId: aws.String("someZoneID123xcv"),
-	}
-
-	_, err := svc.ChangeResourceRecordSets(input)
-	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			case route53.ErrCodeNoSuchHostedZone:
-				fmt.Println(route53.ErrCodeNoSuchHostedZone, aerr.Error())
-			case route53.ErrCodeNoSuchHealthCheck:
-				fmt.Println(route53.ErrCodeNoSuchHealthCheck, aerr.Error())
-			case route53.ErrCodeInvalidChangeBatch:
-				fmt.Println(route53.ErrCodeInvalidChangeBatch, aerr.Error())
-			case route53.ErrCodeInvalidInput:
-				fmt.Println(route53.ErrCodeInvalidInput, aerr.Error())
-			case route53.ErrCodePriorRequestNotComplete:
-				fmt.Println(route53.ErrCodePriorRequestNotComplete, aerr.Error())
-			default:
-				fmt.Println(aerr.Error())
-			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			fmt.Println(err.Error())
-		}
-		return
-	}
 }
